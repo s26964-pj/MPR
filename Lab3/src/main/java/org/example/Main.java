6535318163b0f0da9c8bfe192ae1a2484580e621
s26964-pj <115402943+s26964-pj@users.noreply.github.com>
2023-10-24 23:39:35 +0200

    Update

--- Lab3/src/main/java/org/example/Main.java
+++ Lab3/src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.example.cars.Car;
 import org.example.cars.Type;
+import org.example.services.RentalService;
 import org.example.storage.CarStorage;
 import org.example.rental.Rental;
 import org.example.storage.RentalStorage;
@@ -13,10 +14,24 @@ public class Main {
     public static void main(String[] args) {
         CarStorage.getInstance().addCar(new Car("Fiat", "Bravo","asdasd", Type.STANDARD));
         CarStorage.getInstance().addCar(new Car("Audi","A3", "wafasf", Type.PREMIUM));
-        System.out.println(CarStorage.getInstance().getAllCars());
         User Dominik = new User(1);
 
-        RentalStorage.getInstance().addRental(new Rental(Dominik,CarStorage.getInstance().getAllCars().get(0),new Date(2023,11,13), new Date(2023,12,29)));
-        System.out.println(RentalStorage.getInstance().getAllRental());
+        Date dateFrom = new Date(2023,11,13);
+        Date dateTo = new Date(2023,12,29);
+
+        Date dateFrom1 = new Date(2023,10,13);
+        Date dateTo1 = new Date(2023,12,29);
+
+        Date dateFrom2 = new Date(2023,12,13);
+        Date dateTo2 = new Date(2024,12,29);
+
+        RentalService rentalService = new RentalService();
+        rentalService.rent(Dominik, "asdasd", dateFrom , dateTo);
+        rentalService.isAvailable("asdasd", dateFrom1,dateTo1);
+        rentalService.rent(Dominik, "asdasd", dateFrom1, dateTo1);
+        //todo rentalService.isAvailable(vin, dateFrom, dateTo)
+        //todo rentalService.estimatePrice(vin, dateFrom, dateTo)
+
+
     }
 }
--- Lab3/src/main/java/org/example/services/RentalService.java
+++ Lab3/src/main/java/org/example/services/RentalService.java
@@ -1,6 +1,67 @@
 package org.example.services;
 
+import org.example.cars.Car;
+import org.example.rental.Rental;
+import org.example.storage.CarStorage;
+import org.example.storage.RentalStorage;
+import org.example.user.User;
+
+import java.util.Date;
+
 public class RentalService {
 
+    //todo rent
+    public void rent(User user, String vin, Date dateFrom, Date dateTo) {
+        if()
+        if (isAvailable(vin, dateFrom, dateTo)) {
+            Rental rental = new Rental(user, findCar(vin), dateFrom, dateTo);
+            RentalStorage.getInstance().addRental(rental);
+            System.out.println("Udało się zarezerwowac auto");
+        } else if (!isAvailable(vin, dateFrom, dateTo)) {
+            System.out.println("Auto jest zarezerwowane w tym terminie");
+        }
+
     }
+
+    public Car findCar(String vin) {
+        for (Car car : CarStorage.getInstance().getAllCars()) {
+            if (car.getVin().equals(vin)) {
+                return car;
+            }
+        }
+        return null;
+    }
+
+    //todo isAvilable
+
+    public boolean isAvailable(String vin, Date dateFrom, Date dateTo) {
+        if (findRental(vin) == null) {
+            return false;
+        } else {
+            if (findRental(vin).getEndRental().after(dateTo)) {
+                //System.out.printf("Auto jest zarezerwowane w tym terminie");
+                return false;
+            } else if (findRental(vin).getEndRental().before(dateTo)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Rental findRental(String vin) {
+        for (Car car : CarStorage.getInstance().getAllCars()) {
+            if (car.getVin().equals(vin)) {
+                for (Rental rental : RentalStorage.getInstance().getAllRental()) {
+                    if (rental.getCar().equals(car)) {
+                        return rental;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    //todo estimatePrice
+
+
 }
